5a9f4277a25ccde593b0ff3676af1e61010d3aae
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-11-24 16:44:30 +0200

    create my first server

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+// إنشاء السيرفر
+const myServer = http.createServer((req, response) => {
+    const url = req.url;
+
+    if (url === '/') {
+        response.write('<h1>am backend</h1>');
+        response.end(); // إنهاء الرد
+    } else if (url === '/home') {
+        response.write('<h1>you are in home page</h1>');
+        response.end();
+    } else {
+        // الرد الافتراضي إذا لم يكن الطلب مطابقًا لأي من الحالات السابقة
+        response.write('<h1>404 Not Found</h1>');
+        response.end();
+    }
+});
+
+// استمع للطلبات على المنفذ 8080
+myServer.listen(8080, () => {
+    console.log('server is running on port 8080');
+});
